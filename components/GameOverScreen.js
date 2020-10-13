e9c4b19481f6a7c026b8e4ed9304dd1ce0488531
kboskin <kboskin>
2020-10-13 14:58:56 +0300

    Add images by the URL + refactor end game styling

--- components/GameOverScreen.js
+++ components/GameOverScreen.js
@@ -1,17 +1,24 @@
 import React from "react"
 import { Text, View, StyleSheet, Button, Image } from "react-native"
+import Colors from "../constants/color"
 
 const GameOverScreen = (props) => {
     return(
     <View style={styles.screen}>
-        <Text>Game over, user number : {props.userNumber}</Text>
         <View style={styles.imageContainer}>
             <Image 
-            source={require('../assets/img/image_success.jpg')}
+            fadeDuration={1000}
+            source={{uri:'https://www.incimages.com/uploaded_files/image/1920x1080/getty_495142964_198701.jpg'}}
             style={styles.image}/>
         </View>
-        <Text>Rounds number : {props.roundsCount}</Text>
+        <View style={styles.resultContainer}>
+        <Text style={styles.resultText}>Your phone needed 
+            <Text style={styles.highlightText}> {props.roundsCount} </Text> 
+             rounds to guess the number 
+            <Text style={styles.highlightText}> {props.userNumber} </Text>
+        </Text>
         <View style={styles.restartGameContainer}><Button title="Restart game" onPress={props.restartGameHandler}></Button></View>
+        </View>
     </View>
     )  
 } 
@@ -37,6 +44,22 @@ const styles = StyleSheet.create({
         height: 300,
         borderColor: "black",
         overflow:"hidden"
+    },
+    highlightText: {
+        color: Colors.accentColor,
+        fontFamily: "AriaIn",
+        fontSize: 18
+    },
+    resultContainer: {
+        marginVertical: 40,
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    resultText: {
+        marginHorizontal: 20,
+        fontSize: 22,
+        fontStyle: "italic"
     }
 })
 
